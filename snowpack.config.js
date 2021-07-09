70c231289e7f756eb837b4a1b25f1b9e8236ff9c
ChristBM <36736231+ChristBM@users.noreply.github.com>
2021-07-08 23:23:28 -0400

    Update snowpack.config.js

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: 'https://github.com/ChristBM/workshop1',
+    baseUrl: 'https://christbm.github.io/workshop1/',
   },
 }
